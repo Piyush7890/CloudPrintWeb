fabd17148c705bc450b4f30c136d4181c4fe3270
Piyush7890 <mamidwar.piyush@gmail.com>
2019-03-31 10:33:05 +0530

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {LoginComponent} from './login/login.component';
+import {
+  MdcCardModule,
+  MdcFormFieldModule,
+  MdcButtonModule,
+  MdcFabModule,
+  MdcChipsModule,
+  MdcIconModule,
+  MdcTextFieldModule,
+} from '@angular-mdc/web';
+import {SignupComponent} from './signup/signup.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
+    MdcCardModule,
+    MdcTextFieldModule,
+    MdcFormFieldModule,
+    MdcChipsModule,
+    MdcButtonModule,
+    MdcFabModule,
+    MdcIconModule,
     BrowserModule,
     AppRoutingModule
   ],
